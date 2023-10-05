d52af7bb6c0880714704a5ef0e56f7912502de13
Ale <alejandra.piedra02@gmail.com>
2023-10-04 19:50:07 -0600

    Corrección del script app.js y index.html para poder echar a andar la API

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,38 +1,48 @@
 
-//En este script se recupera una API para poder traer información de distintos usuarios del JSON de Github users, dentro de todos los que tiene para recuperar al usuario stolinski. En lo que se recupera, muestra un mensaje de Cargando. En el caso de que no lo recupere, mostrará un mensaje error. 
+//En este script se recupera una API para poder traer información de distintos usuarios del JSON de Github users. En este caso, de stolinski.En lo que se recupera, muestra un mensaje de Cargando. En el caso de que no lo recupere, mostrará un mensaje error. 
 
-//Se está trabajando en la ejecución del programa
-
-// Se agregó el fetch para hacer la conexión con la API de Github
-const baseEndpoint = fetch("https://api.github.com/users");
 //Para refactorizar, se elimina la variable EndUser ya que sería directamente a añadirle ese texto al primer endPoint declarado, y al final mejor que se sume el nombre del usuario
+const baseEndpoint = ('https://api.github.com/users');
 
-// Se reciben los elementos de nuestro HTML, por lo que hay que referenciarlos correctamente con su respectivo getElement
-// Se observa que el blog se recupera a través de un ID, por lo que se elimina del parámetro del getElementById y a su vez se actualiza en HTML.
-const $n = document.getElementById("name");
-const $b = document.getElementById("blog");
-const $l = document.getElementById("location");
 
-// El await se utiliza con funciones async, por lo que se le agregó a la función
+// Se corrigen las incosistencias de recuperar los elementos mediante el #
+const $n = document.querySelector('.name');
+const $b = document.querySelector('.blog');
+const $l = document.querySelector('.location');
+
+// El await se utiliza con funciones async, por lo que se le agregó a la función. Para hacer la llamada usaremos el async/await
 async function displayUser(username) {
-  $n.textContent = 'cargando...';
+  //En lo que carga la información, mostrará el mensaje de cargando
+  $n.textContent = `cargando...`;
+  $b.textContent = `cargando...`;
+  $l.textContent = `cargando...`;
 
-  const response = await fetch(`${baseEndpoint}/${username}`);
-  console.log(data);
+ 
+  // Usamos el try para hacer las dos llamadas
+  try{
+    const response = await fetch(`${baseEndpoint}/${username}`);
+    const data = await response.json();
+    console.log(data);
 
-  // Se cambian las comillas simples a backsticks para poder leer las variables
-  $n.textContent = `${data.name}`;
-  $b.textContent = `${data.blog}`;
-  $l.textContent = `${data.location}`;
+    // Se cambian las comillas simples a backsticks para poder leer las variables
+    $n.textContent = `${data.name}`;
+    $b.textContent = `${data.blog}`;
+    $l.textContent = `${data.location}`;
+  }catch(err){
+    handleError();
+  }
+
 }
 
-// Mensaje para mostrar en consola que no se pudo hacer la conexión con la API
+// Mensaje para mostrar en consola que no se pudo hacer la conexión con la API en dado caso
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
 
-  //Se añadió el $ a la variable n, para hacer el match con su declaración
+  //Se añadió el $ a la variable n, para hacer el match con su declaración al inicio del script, y así traspasar la información al HTML en dado caso.
   $n.textContent = `Algo salió mal: ${err}`;
+  $b.textContent = `Algo salió mal: ${err}`;
+  $l.textContent = `Algo salió mal: ${err}`;
 }
 
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski');
